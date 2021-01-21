7744e212bc622424d864e902f5d97f8d67acf5e7
Mayank Chaudhari <mayank.srmu@gmail.com>
2021-01-21 14:04:31 +0530

    fix: stringify object before saving
    localStorage does not support complex objects. stringify contacts array before saving
    added methods for updating contacts

--- util/contacts.tsx
+++ util/contacts.tsx
@@ -1,18 +1,36 @@
-export const getContacts = () => localStorage.contacts || [];
+const getCtx = () => {
+  return JSON.parse(localStorage.contacts || "[]");
+};
+
+export const getContacts = () =>
+  new Promise((resolve, reject) => resolve(getCtx()));
 
 export const getContact = (id: Number | String) => {
-  const c = getContacts().filter((it) => it.id == id);
-  return c.length ? c[0] : null;
+  const c = getCtx().filter((it) => it.id == id);
+  return new Promise((resolve, reject) =>
+    c.length ? resolve(c[0]) : reject("Contact Not Found")
+  );
 };
 
 export const createContact = (body) => {
-  const c = getContacts();
+  const c = getCtx();
   const l = c.length;
   const id = (l == 0 ? 0 : c[l - 1].id) + 1;
   const contact = {
     ...body,
     id,
   };
-  localStorage.contacts = c.concat([contact]);
-  return localStorage.contacts;
+  localStorage.contacts = JSON.stringify(c.concat([contact]));
+  return new Promise((resolve, reject) => resolve(true));
+};
+
+export const updateContact = (contact) => {
+  const c = getCtx().map((ct) => (ct.id == contact.id ? contact : ct));
+  localStorage.contacts = JSON.stringify(c);
+  return new Promise((resolve, reject) => resolve(true));
+};
+
+export const removeContact = (id) => {
+  localStorage.contacts = JSON.stringify(getCtx().filter((it) => it.id != id));
+  new Promise((resolve, reject) => resolve(JSON.parse(localStorage.contacts)));
 };
